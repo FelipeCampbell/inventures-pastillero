0cfdeb5e36a112c7533b9b61712e03e8fe90ed78
FelipeCampbell <fcampbell@uc.cl>
2022-11-30 16:13:44 -0300

    chore(react-query): add react query providers to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "@next/font/google";
+import { useState } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600"],
 });
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={poppins.className}>
-      <Component {...pageProps} />
-    </main>
+    <QueryClientProvider client={queryClient}>
+        <main className={poppins.className}>
+          <Component {...pageProps} />
+        </main>
+    </QueryClientProvider>
   );
 }
